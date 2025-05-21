375960a811d16c336fb69402ff1f4e64bac4fb53
Kravets Rustam aka Raspredval <rustKravets@gmail.com>
2025-05-21 13:10:38 +0700

    redesigned macro api, to account for builtin macro custom behavior

--- src/lua/Preprocessor/IMacro.lua
+++ src/lua/Preprocessor/IMacro.lua
@@ -1,22 +1,14 @@
-local oop = require "oop"
+local oop, log =
+    require "oop",
+    require "log"
 
 ---@class IMacro
 local IMacro = oop.newClass()
 
+---@param ... string
 ---@return string
-function IMacro:Body()
-    error("invalid abstract method call", 2)
-end
-
----@param i integer
----@return string?
-function IMacro:ParamName(i)
-    return nil
-end
-
----@return integer
-function IMacro:ParamCount()
-    return 0
+function IMacro:Expand(...)
+    return ""
 end
 
 return IMacro
