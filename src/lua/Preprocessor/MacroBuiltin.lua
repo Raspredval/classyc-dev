219bd88297b593c9add052dd37660528e9c1c38d
Kravets Rustam aka Raspredval <rustKravets@gmail.com>
2025-05-22 13:18:48 +0700

    changed MacroBuiltin api since base IMacro class api was changed

--- src/lua/Preprocessor/MacroBuiltin.lua
+++ src/lua/Preprocessor/MacroBuiltin.lua
@@ -2,26 +2,31 @@ local oop, IMacro =
     require "oop",
     require "Preprocessor.IMacro"
 
+---@alias OnExpandFunc fun(self: Preprocessor, objFileInfo: FileInfo, tblMacros: MacroLookupTable, ... : string) : string
+
 ---@class MacroBuiltin : IMacro
 ---@field private objPreprocessor Preprocessor
----@field private fnGetBody fun(obj: Preprocessor) : string
+---@field private fnOnExpand OnExpandFunc
 local MacroBuiltin = oop.newClass(IMacro)
 
 ---@param objPreprocessor Preprocessor
----@param fnGetBody fun(obj: Preprocessor) : string
+---@param fnOnExpand OnExpandFunc
 ---@return MacroBuiltin
-function MacroBuiltin.New(objPreprocessor, fnGetBody)
+function MacroBuiltin.New(objPreprocessor, fnOnExpand)
     local obj   = setmetatable({
                     objPreprocessor = objPreprocessor,
-                    fnGetBody = fnGetBody
+                    fnOnExpand      = fnOnExpand
                 }, MacroBuiltin)
 
     return obj
 end
 
+---@param objFileInfo FileInfo
+---@param tblMacros MacroLookupTable
+---@param ... string
 ---@return string
-function MacroBuiltin:Body()
-    return self.fnGetBody(self.objPreprocessor)
+function MacroBuiltin:Expand(objFileInfo, tblMacros, ...)
+    return self.fnOnExpand(self.objPreprocessor, objFileInfo, tblMacros, ...)
 end
 
 return MacroBuiltin
