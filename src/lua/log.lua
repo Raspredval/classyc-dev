1b2f39616cf3400c4b5982c8937a9fba35b248f2
Kravets Rustam aka Raspredval <rustKravets@gmail.com>
2025-05-22 11:31:39 +0700

    slightly changed logging functions, moved soruce logging to FileInfo

--- src/lua/log.lua
+++ src/lua/log.lua
@@ -12,8 +12,27 @@ end
 
 ---@param strfmt string
 ---@param ... any
+function log.error_no_trace(strfmt, ...)
+    error("Error: " .. strfmt:format(...), 0)
+end
+
+---@generic T
+---@param v T?
+---@param strfmt string
+---@param ... any
+---@return T
+function log.assert_no_trace(v, strfmt, ...)
+    if not v then
+        error("Assertion failed: " .. strfmt:format(...), 0)
+    end
+
+    return v
+end
+
+---@param strfmt string
+---@param ... any
 function log.error(strfmt, ...)
-    error(strfmt:format(...), 0)
+    error("Error: " .. strfmt:format(...), 2)
 end
 
 ---@generic T
@@ -23,51 +42,7 @@ end
 ---@return T
 function log.assert(v, strfmt, ...)
     if not v then
-        log.error(strfmt, ...)
-    end
-
-    return v
-end
-
----@param strSourceFile string
----@param nSourceLine integer
----@param strfmt string
----@param ... any
-function log.source_error(strSourceFile, nSourceLine, strfmt, ...)
-    log.error(("[%s:%i] Error: %s") :
-        format(strSourceFile, nSourceLine, strfmt), ...)
-end
-
----@generic T
----@param strSourceFile string
----@param nSourceLine integer
----@param v T?
----@param strfmt string
----@param ... any
----@return T
-function log.source_assert(strSourceFile, nSourceLine, v, strfmt, ...)
-    if not v then
-        log.source_error(strSourceFile, nSourceLine, strfmt, ...)
-    end
-
-    return v
-end
-
----@param strfmt string
----@param ... any
-function log.internal_error(strfmt, ...)
-    log.error(("%s\n Internal error: %s") :
-        format(debug.traceback("traceback", 2), strfmt))
-end
-
----@generic T
----@param v T?
----@param strfmt string
----@param ... any
----@return T
-function log.internal_assert(v, strfmt, ...)
-    if not v then
-        log.internal_error(strfmt, ...)
+        error("Assertion failed: " .. strfmt:format(...), 2)
     end
 
     return v
