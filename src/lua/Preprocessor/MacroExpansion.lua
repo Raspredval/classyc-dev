3dd874b73f052b767631ab04ef3573709a7b2686
Kravets Rustam aka Raspredval <rustKravets@gmail.com>
2025-05-22 12:55:22 +0700

    moved Preprocessor.ExpandMacros into a separate class

---
+++ src/lua/Preprocessor/MacroExpansion.lua
@@ -0,0 +1,65 @@
+local oop, log, lpeg, PEG, FileInfo, IMacro =
+    require "oop",
+    require "log",
+    require "lpeg",
+    require "CommonPatterns",
+    require "FileInfo",
+    require "Preprocessor.IMacro"
+
+---@diagnostic disable missing-fields
+
+---@type Pattern
+local pegParseMacro = lpeg.P{
+    "first_macro",
+    text        = (lpeg.P(1) - "$" - "\"" - "\'") ^ 0,
+    literal     = PEG.StringLiteral + PEG.CharLiteral,
+    macro       = "$" * lpeg.C(PEG.ID) * lpeg.Ct(("(" * PEG.ExprList * ")") ^ -1),
+    first_macro = lpeg.V"text" * (lpeg.V"literal" * lpeg.V"text") ^ 0 * PEG.Bounds(lpeg.V"macro")
+}
+
+---@diagnostic enable missing-fields
+
+---@alias MacroLookupTable table<string, IMacro>
+
+---@class MacroExpansion
+---@field private objFileInfo       FileInfo
+---@field private tblMacros         MacroLookupTable
+local MacroExpansion = oop.newClass()
+
+---@param objFileInfo FileInfo
+---@param tblMacros MacroLookupTable
+---@return MacroExpansion
+function MacroExpansion.New(objFileInfo, tblMacros)
+    local obj   = setmetatable({
+                    objFileInfo = objFileInfo,
+                    tblMacros   = tblMacros,
+                }, MacroExpansion)
+
+    return obj
+end
+
+---@param strChunk string
+---@return string
+function MacroExpansion:ExpandMacros(strChunk)
+    while true do
+        local nMacroBegin, strMacroName, tblMacroParams, nMacroEnd =
+            pegParseMacro:match(strChunk)
+        
+        if not strMacroName then
+            return strChunk
+        else
+            local objMacro  = log.assert(self.tblMacros[strMacroName],
+                                "undefined macro: %s", strMacroName)
+            log.assert(oop.isRelatedTo(objMacro, IMacro),
+                "'%s' macro name is reserved and cannot be used")
+
+            strChunk = ("%s%s%s"):format(
+                strChunk:sub(1, nMacroBegin - 1),
+                objMacro:Expand(unpack(tblMacroParams)),
+                strChunk:sub(nMacroEnd))
+        end
+    end
+end
+
+return MacroExpansion
+
