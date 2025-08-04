89e07fd34e8d8f175a187141dc6118280fd11938
Kravets Rustam aka Raspredval <rustKravets@gmail.com>
2025-08-04 11:46:15 +0700

    changed error message formatting a bit

--- lua/log.lua
+++ lua/log.lua
@@ -12,27 +12,8 @@ end
 
 ---@param strfmt string
 ---@param ... any
-function log.error_no_trace(strfmt, ...)
-    error("Error: " .. strfmt:format(...), 0)
-end
-
----@generic T
----@param v T?
----@param strfmt string
----@param ... any
----@return T
-function log.assert_no_trace(v, strfmt, ...)
-    if not v then
-        error("Assertion failed: " .. strfmt:format(...), 0)
-    end
-
-    return v
-end
-
----@param strfmt string
----@param ... any
 function log.error(strfmt, ...)
-    error("Error: " .. strfmt:format(...), 2)
+    error("Error: " .. strfmt:format(...), 0)
 end
 
 ---@generic T
@@ -42,7 +23,7 @@ end
 ---@return T
 function log.assert(v, strfmt, ...)
     if not v then
-        error("Assertion failed: " .. strfmt:format(...), 2)
+        log.error(strfmt, ...)
     end
 
     return v
