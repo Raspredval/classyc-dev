7d36128fc9e4c7b840b42acf3c167f8fd24a231f
Kravets Rustam aka Raspredval <rustKravets@gmail.com>
2025-08-07 15:14:30 +0700

    added fs.stat fs.FileStats api to the meta file

--- lua/meta/FilesystemAPI.lua
+++ lua/meta/FilesystemAPI.lua
@@ -54,4 +54,57 @@ function Directory:telldir() end
 
 function Directory:rewinddir() end
 
+---@param path string
+---@return FileStats?
+function fs.stat(path) end
+
+---@class FileStats
+local FileStats = {}
+
+---@return integer
+function FileStats:getDeviceID() end
+
+---@return integer
+function FileStats:getFileID() end
+
+---@return integer
+function FileStats:getOwnerID() end
+
+---@return integer
+function FileStats:getGroupID() end
+
+---@return string?
+function FileStats:getOwnerName() end
+
+---@return string?
+function FileStats:getGroupName() end
+
+---@return integer
+function FileStats:getLastAccessTime() end
+
+---@return integer
+function FileStats:getLastModificationTime() end
+
+---@return integer
+function FileStats:getLastStatusChangeTime() end
+
+---@return integer
+function FileStats:getFileSize() end
+
+---@return "file" | "directory" | "pipe" | "symbolic link" | "socket" | "block special" | "char special" | "unknown"
+function FileStats:getFileType() end
+
+
+---return values: read bit, write bit, execute bit
+---@return boolean, boolean, boolean
+function FileStats:getOwnerPermissions() end
+
+---return values: read bit, write bit, execute bit
+---@return boolean, boolean, boolean
+function FileStats:getGroupPermissions() end
+
+---return values: read bit, write bit, execute bit
+---@return boolean, boolean, boolean
+function FileStats:getOtherPermissions() end
+
 return fs
