9f7bf51c82ed1e71ef9a939a542a65f0e1cecc75
Raspredval <rustKravets@gmail.com>
2025-10-20 10:30:15 +0700

    testing patterns

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,41 @@
 #include <my-iostreams/ConsoleStreams.hpp>
+#include <my-iostreams/BufferStreams.hpp>
 #include <my-patterns/Patterns.hpp>
 
 int
 main() {
-    io::cout.Fmt("Hello, world!").Endl();
+    patt::Grammar
+        gr;
+
+    gr["filename"]  =
+        (patt::Str("..") |= patt::Str(".") |= (patt::Alnum() % 1)) >>
+        (patt::Str("/") >> gr["filename"]) % -1;
+    patt::Pattern
+        ptFilepath  =
+            (patt::Str("/") |= patt::Str("~/")) % -1 >>
+            gr["filename"] % -1 >>
+            patt::Endl();
+
+    io::IOBufferStream
+        buffTest;
+
+    std::string
+        strInput;
+    io::cin.Line(strInput);
+    io::cout.Fmt("input: \"{}\"\n", strInput);
+
+    io::TextWriter(buffTest)
+        .Str(strInput)
+        .Str("\r\n")
+        .GoStart();
+
+    auto
+        optMatch    = patt::SafeEval(ptFilepath, buffTest);
+    if (!optMatch) {
+        io::cerr.Str(optMatch.error().what()).Endl();
+        return -1;
+    }
+
+    io::cout.Fmt("matched: \"{}\"\n", optMatch->GetString(buffTest));
+    return 0;
 }
