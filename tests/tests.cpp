9e4c71295e9392bc65627b8f6844440077363e2f
Raspredval <rustKravets@gmail.com>
2025-10-21 12:07:28 +0700

    fixed tests/tests.cpp and cmake build script to support multiple tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,6 +2,22 @@
 #include "TestTextIO.hpp"
 
 int
-main() {
-    TestTextIO();
+main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        io::cerr.fmt("invalid arg count, expected 1, got {}", argc - 1);
+        return EXIT_FAILURE;
+    }
+
+    std::string_view
+        strvTest    = argv[1];
+    if (strvTest == "--patterns")
+        TestPatterns();
+    else if (strvTest == "--text-io")
+        TestTextIO();
+    else {
+        io::cerr.fmt("unrecognized test: {}", strvTest);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
