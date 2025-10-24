0a27ec0079f9ccdcdda2546767513eb681e87fd6
Raspredval <rustKravets@gmail.com>
2025-10-24 10:33:36 +0700

    added some arg validation for create_symlink.cmake, need to test it next

--- create_symlink.cmake
+++ create_symlink.cmake
@@ -1,3 +1,14 @@
+macro(assert TEST_CONDITION MESSAGE)
+  if(NOT ${TEST_CONDITION})
+    message(FATAL_ERROR "${MESSAGE}")
+  endif()
+endmacro()
+
+assert(
+    "${CMAKE_ARGV1}" STREQUAL "-P" AND
+    "${CMAKE_ARGV2}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}/create_symlimk.cmake",
+    "Invalid create_symlink arguments!")
+
 set(LINK_SRC    ${CMAKE_ARGV3})
 set(LINK_DEST   ${CMAKE_ARGV4})
 
