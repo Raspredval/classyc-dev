3f4a27a534769937116f8185bfc8a1c36d17ae0c
Raspredval <rustKravets@gmail.com>
2025-11-03 21:41:25 +0700

    tested the updated pattern grammar

--- tests/include/TestPatterns.hpp
+++ tests/include/TestPatterns.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "my-iostreams/TextIO.hpp"
+#include <cstdlib>
 #include <map>
 #include <string>
 
@@ -17,79 +19,33 @@ inline int
 TestPatterns() {
     io::cout.put("Testing Patterns:\n");
 
-    std::map<std::string, std::string>
-        mapMacros;
-    MacroArgs
-        args;
-    size_t
-        uLineCount      = 1;
-    
-    auto
-        fnHandleDefine  =
-            [&args, &mapMacros] (io::IStream&, const std::optional<patt::Match>& optMatch) {
-                if (!optMatch)
-                    return;
-
-                mapMacros.emplace(
-                    std::move(args.strName),
-                    std::move(args.strValue));
-            };
-    auto
-        fnCountLines    =
-            [&uLineCount] (io::IStream&, const std::optional<patt::Match>& optMatch) {
-                if (optMatch)
-                    uLineCount += 1;
-            };
-
-    patt::Pattern
-        ptEndl          = patt::Str("\n") |= patt::Str("\r\n") |= patt::None(),
-        ptSpacing       = patt::Space() % 1,
-        ptIdentifier    =
-            (patt::Alpha() |= patt::Str("_")) >>
-            (patt::Alnum() |= patt::Str("_")) % 0;
-    patt::Pattern
-        ptCmdDefine     =
-            patt::Str("#define") >> ptSpacing >>
-            ptIdentifier / args.strName >> ptSpacing >>
-            (-ptEndl >> patt::Any()) % 1 / args.strValue >>
-            ptEndl / fnCountLines;
-    patt::Pattern
-        ptCommands      =
-            (ptCmdDefine / fnHandleDefine) % 0 >> patt::None();
-        
-    io::IFileStream
-        fileSource("./assets/input.txt");
-
-    std::optional<patt::Match>
-        optMatch    = patt::Eval(ptCommands, fileSource);
-    if (!optMatch) {
-        io::cerr.fmt("line {}: failed to match\n", uLineCount);
-        return EXIT_FAILURE;
-    }
-
-    for (const auto& [strKey, strValue] : mapMacros) {
-        io::cout.fmt("[{}]:\t\"{}\"\n", strKey, strValue);
-    }
-
-    patt::Pattern
-        ptFooBar    = patt::Str("foo") >> &patt::Str("bar"),
-        ptBarBuz    = patt::Str("bar") >> &patt::Str("buz"),
-        ptBuzzKill  = patt::Str("buzzkill");
     io::IOBufferStream
         buffTest;
     io::TextIO(buffTest)
-        .put("foobarbuzzkill")
+        .put_str("std.chrono.duration")
         .go_start();
 
-    std::optional<patt::Match>
-        optWordMash = patt::Eval(ptFooBar >> ptBarBuz >> ptBuzzKill, buffTest);
-    if (!optWordMash) {
-        io::cerr.put("failed to match word mash\n");
+    patt::Grammar
+        gr;
+    patt::Pattern
+        ptID        = (patt::Str("_") |= patt::Alpha()) >> (patt::Str("_") |= patt::Alnum()) % 1;
+
+    std::vector<std::string>
+        vecNames;
+    gr["name"]      = ptID / vecNames >> (patt::Str(".") >> gr["name"]) % -1;
+
+    auto
+        optMatch    = patt::Eval(gr["name"], buffTest);
+    if (optMatch) {
+        io::cout.fmt("name parts:\n");
+        for (const auto& strName : vecNames) {
+            io::cout.fmt("\t{}\n", strName);
+        }
+
+        return EXIT_SUCCESS;
+    }
+    else {
+        io::cerr.put_str("failed to parse full name\n");
         return EXIT_FAILURE;
     }
-
-    io::cout.fmt("matched \"{}\"\n",
-        optWordMash->GetString(buffTest));
-
-    return EXIT_SUCCESS;
 }
